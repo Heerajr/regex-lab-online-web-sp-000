123b079b6f8f5c61948f040e546ec20508230942
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-04-13 00:42:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,6 +25,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/[0-9].+[0-9].+[0-9]/)
+  phone.match(/[0-9].+[0-9].+[0-9]/)? true : false 
 
 end
